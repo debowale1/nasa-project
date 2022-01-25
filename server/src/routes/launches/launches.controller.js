e8283c5251ff7669724a84f69c2c2276725c24c1
Adeshina Ibrahim <debowale01@gmail.com>
2022-01-25 11:21:48 +0100

    launch input validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,6 +6,17 @@ function httpGetAllLaunches(req, res){
 
 function httpAddNewLaunch(req, res){
   const launch = req.body
+  if(!launch.mission || !launch.rocket || !launch.destination || !launch.launchDate ){
+    return res.status(400).json({
+      error: 'Invalid Launch Paramaters'
+    })
+  }
+
+  if(isNaN(launch.launchDate)){
+    return res.status(400).json({
+      error: "Invalid launch date"
+    })
+  }
   launch.launchDate = new Date(launch.launchDate)
   addNewLaunch(launch)
   return res.status(201).json(launch)
