68bda0104346c0116aaaae8a4d65bd0d8b04dae3
Adeshina Ibrahim <debowale01@gmail.com>
2022-01-25 15:55:35 +0100

    launch abortion

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,7 @@ function httpGetAllLaunches(req, res){
 
 function httpAddNewLaunch(req, res){
   const launch = req.body
-  if(!launch.mission || !launch.rocket || !launch.destination || !launch.launchDate ){
+  if(!launch.mission || !launch.rocket || !launch.target || !launch.launchDate ){
     return res.status(400).json({
       error: 'Invalid Launch Paramaters'
     })
@@ -22,7 +22,12 @@ function httpAddNewLaunch(req, res){
   return res.status(201).json(launch)
 }
 
+function httpAbortLaunch(req, res){
+  const launchId = req.params.id;
+}
+
 module.exports = {
   httpGetAllLaunches,
-  httpAddNewLaunch
+  httpAddNewLaunch,
+  httpAbortLaunch
 }
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { httpGetAllLaunches, httpAddNewLaunch } = require('./launches.controller')
+const { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch } = require('./launches.controller')
 
 const launchesRouter = express.Router()
 
 launchesRouter.route('/').get(httpGetAllLaunches).post(httpAddNewLaunch)
+launchesRouter.route('/:id').delete(httpAbortLaunch)
 
 
 module.exports = launchesRouter
